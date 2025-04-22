b599a244e7eb76032c008483be11695dc02b4f86
DK <dk@vrittechnologies.com>
2025-04-22 11:04:47 +0545

    solved the deployment issue in frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Home from "./pages/Home";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Success from "./pages/Success";
 const App = () => {
   return (
     <div>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/success" element={<Success />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,9 @@
-// index.jsx or main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import Success from "./pages/Success";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/success" element={<Success />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
